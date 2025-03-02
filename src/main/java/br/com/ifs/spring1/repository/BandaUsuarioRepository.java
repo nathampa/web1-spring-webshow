4fa30e43819b98a0cab2ac7fe1dd0e2a5149b8af
Nathan Ferreira <ferreiragfx@gmail.com>
2025-03-02 19:36:47 -0300

    Corrigindo metodo de remover usuario da banda

--- src/main/java/br/com/ifs/spring1/repository/BandaUsuarioRepository.java
+++ src/main/java/br/com/ifs/spring1/repository/BandaUsuarioRepository.java
@@ -4,7 +4,9 @@ import br.com.ifs.spring1.model.Banda;
 import br.com.ifs.spring1.model.BandaUsuario;
 import br.com.ifs.spring1.model.BandaUsuarioId;
 import br.com.ifs.spring1.model.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,7 +16,10 @@ import java.util.List;
 @Repository
 public interface BandaUsuarioRepository extends JpaRepository<BandaUsuario, BandaUsuarioId> {
     boolean existsByIdBandaAndIdUsuario(Integer idBanda, Integer idUsuario);
+    @Modifying
+    @Transactional
     void deleteByIdBandaAndIdUsuario(Integer idBanda, Integer idUsuario);
+
     /*@Query(value = "SELECT b FROM bandas b WHERE b.id_banda IN (SELECT bu.id_banda FROM banda_usuario bu WHERE bu.id_usuario = :usuarioId) ORDER BY b.nome ASC",
             nativeQuery = true)
     List<Banda> findBandasByUsuarioIdOrderByNome(@Param("usuarioId") Integer usuarioId);*/
